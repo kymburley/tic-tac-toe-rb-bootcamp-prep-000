96cfd0004c45ae64e1d528f3cf1eb798403d62cb
Kimberly Mozzetti <kimberly.mozzetti@gmail.com>
2017-12-29 13:57:00 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,10 @@
-
+WIN_COMBINATIONS = [
+  [0, 1, 2], # top_row_win
+  [3, 4, 5], # middle_row_win
+  [6, 7, 8], # bottom_row_win
+  [0, 3, 6], # first_column_win
+  [1, 4, 7], # middle_column_win
+  [2, 5, 8], # last_column_win
+  [0, 4, 8], # first_diagonal_win
+  [2, 4, 6]  # second_diagonal_win
+]
