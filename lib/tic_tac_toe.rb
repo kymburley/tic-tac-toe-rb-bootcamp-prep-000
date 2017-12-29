1360fa2c453473e068789430c3d0bf7289fdf170
Kimberly Mozzetti <kimberly.mozzetti@gmail.com>
2017-12-29 14:18:56 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -56,7 +56,7 @@ def turn(board)
   index = input_to_index(input)
 
   if valid_move?(board, index) == true
-    move(board, index)
+    move(board, index, player)
     display_board(board)
   else
     turn(board)
