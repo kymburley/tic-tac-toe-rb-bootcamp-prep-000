5589e6d85a6b48128ff6b270fbc3afa0df842cbe
Kimberly Mozzetti <kimberly.mozzetti@gmail.com>
2017-12-29 14:11:47 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,3 +32,11 @@ end
 def move(board, index, player)
   board[index] = player
 end
+
+def position_taken?(board, index)
+  if (board[index] == "" || board[index] == " " || board[index] == nil)
+    false
+  elsif (board[index] == "X" || board[index] == "O")
+    true
+  end
+end
