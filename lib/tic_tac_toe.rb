97edeec3c77b38b4daccafe33ccecee4ebc1f077
Kimberly Mozzetti <kimberly.mozzetti@gmail.com>
2017-12-29 14:14:25 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -40,3 +40,11 @@ def position_taken?(board, index)
     true
   end
 end
+
+def valid_move?(board, index)
+  if position_taken?(board, index) == false && index.between?(0, 8)
+    true
+  else
+    false
+  end
+end
